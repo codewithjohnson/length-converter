808aa2abf7e9790a786d5a5a9896c576bc3a3b32
codewithjohnson <75730906+codewithjohnson@users.noreply.github.com>
2022-06-27 00:51:51 +0100

    commit

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 const selectInputs = document.querySelectorAll('select');
-console.log(selectInputs);
 
-for (let)
+const units = [
+    'meter',
+    'inches',
+    'feet',
+    'kilometers',
+    'yards',
+    'miles'
+];
+
+for (let i = 5; i >= 0; i--) {
+    selectInputs[0].firstElementChild.insertAdjacentHTML('afterend', `<option value= ${units[i]}>${units[i]}</option>`);
+}
+
+for (let i = 5; i >= 0; i--) {
+    selectInputs[1].firstElementChild.insertAdjacentHTML('afterend', `<option value= ${units[i]}>${units[i]}</option>`);
+}
--- main.js
+++ main.js
@@ -4,22 +4,42 @@ const convertTo = document.querySelector('#to');
 const formulaContent = document.querySelector('.formular_content');
 const result = document.querySelector('#result');
 
+const hints = [
+    {
+        sameUnit: 'gives the same value',
+    },
 
-function convertValues(){
+    {
+        c_m:'Divide the length by 100',
+    },
+    
+    {
+        m_c:'Multiply the length by 100',
+    }
+];
+
+
+
+
+function convertValues() {
     if ((convertFrom.value === 'centimeter') && (convertTo.value === 'centimeter')) {
         result.value = length.value;
+        formulaContent.textContent = hints[0].sameUnit;
     } 
-
+    
     else if ((convertFrom.value === 'meter') && (convertTo.value === 'meter')) {
         result.value = length.value;
+        formulaContent.textContent = hints[0].sameUnit;
     } 
     
     else if ((convertFrom.value === 'centimeter') && (convertTo.value === 'meter')) {
         result.value = length.value / 100;
-    }
-
+        formulaContent.textContent = hints[1].c_m;
+    } 
+    
     else if ((convertFrom.value === 'meter') && (convertTo.value === 'centimeter')) {
         result.value = length.value / 100;
+        formulaContent.textContent = hints[2].m_c;
     }
 }
 
@@ -28,4 +48,4 @@ function convertValues(){
 
 convertFrom.addEventListener('change', convertValues);
 convertTo.addEventListener('change', convertValues);
-length.addEventListener('input',convertValues);
+length.addEventListener('input', convertValues);
