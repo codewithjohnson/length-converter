52d5e42c85459aac18c35c58729080efb6f8def3
codewithjohnson <75730906+codewithjohnson@users.noreply.github.com>
2022-07-02 10:44:04 +0100

    ok

--- main.js
+++ main.js
@@ -5,10 +5,9 @@ const formulaContent = document.querySelector('.formular_content');
 const result = document.querySelector('#result');
 
 
-const hints = [
-    {
+const hints = [{
         // 0
-        sameUnit: 'gives the same value' 
+        sameUnit: 'gives the same value'
     },
 
     {
@@ -16,48 +15,48 @@ const hints = [
         c_m: 'Divide the length by 100'
     },
     {
-        // 2
-        c_f:'Multiply the length by 0.032808'
+        // 2 CENTIMETER TO FEET
+        c_f: 'Multiply the length by 0.032808'
     },
     {
-        // 3
-        c_i:'Multiply the length by 0.39370'
+        // 3 CENTIMETER TO INCHES
+        c_i: 'Multiply the length by 0.39370'
     },
     {
-        // 4
-        c_y:'Multiply the length by 0.010936'
+        // 4 CENTIMETER TO YARDS
+        c_y: 'Multiply the length by 0.010936'
     },
     {
-        // 5
-        c_km:'Divide the length by 100000'
+        // 5 CENTIMETER TO KILOMETER
+        c_km: 'Divide the length by 100000'
     },
     {
-        // 6
-        c_mi:'Multiply the length by 0.0000062137'
+        // 6 CENTIMETER TO MILES
+        c_mi: 'Multiply the length by 0.0000062137'
     },
     {
-        // 7
-        m_c:'Multiply the length by 100'
+        // 7 METER TO CENTIMETER
+        m_c: 'Multiply the length by 100'
     },
     {
-        // 8
-        m_f:'Multiply the length by 3.2808'
+        // 8 METER TO FEET
+        m_f: 'Multiply the length by 3.2808'
     },
     {
-        // 9
-        m_y:'Multiply the length by 1.0936'
+        // 9 METER TO YARDS
+        m_y: 'Multiply the length by 1.0936'
     },
     {
-        // 10
-        m_mi:'Multiply the length by 0.00062137'
+        // 10 METER TO MILES
+        m_mi: 'Multiply the length by 0.00062137'
     },
     {
-        // 11
-        m_i:'Multiply the length by 39.370'
+        // 11 METER TO INCHES
+        m_i: 'Multiply the length by 39.370'
     },
     {
-        // 12
-        m_km:'Divide the length by 1000'
+        // 12 METER TO KILOMETER
+        m_km: 'Divide the length by 1000'
     },
 ];
 
@@ -77,35 +76,30 @@ function convertValues() {
         result.style.border = '2px solid red';
         result.value = length.value;
         formulaContent.textContent = hints[0].sameUnit;
-    } 
+    }
     // CENTIMETER CONVERSION
     else if ((convertFrom.value === 'centimeter') && (convertTo.value === 'meter')) {
         result.value = length.value / 100;
         formulaContent.textContent = hints[1].c_m;
         result.style.border = '3px solid green';
-    } 
-    else if ((convertFrom.value === 'centimeter') && (convertTo.value === 'feet')) {
+    } else if ((convertFrom.value === 'centimeter') && (convertTo.value === 'feet')) {
         result.value = length.value * 0.032808;
         formulaContent.textContent = hints[2].c_f;
         result.style.border = '3px solid green';
-    }
-    else if ((convertFrom.value === 'centimeter') && (convertTo.value === 'inches')) {
+    } else if ((convertFrom.value === 'centimeter') && (convertTo.value === 'inches')) {
         result.value = length.value * 0.39370;
         formulaContent.textContent = hints[3].c_i;
         result.style.border = '3px solid green';
-    }
-    else if ((convertFrom.value === 'centimeter') && (convertTo.value === 'yards')) {
+    } else if ((convertFrom.value === 'centimeter') && (convertTo.value === 'yards')) {
         result.value = length.value * 0.010936;
         formulaContent.textContent = hints[4].c_y;
         result.style.border = '3px solid green';
-    }
-    else if ((convertFrom.value === 'centimeter') && (convertTo.value === 'kilometers')) {
+    } else if ((convertFrom.value === 'centimeter') && (convertTo.value === 'kilometers')) {
         result.value = length.value / 100000;
         formulaContent.textContent = hints[5].c_km;
         result.style.border = '3px solid green';
-    }
-    else if ((convertFrom.value === 'centimeter') && (convertTo.value === 'miles')) {
-        result.value = length.value * 0.0000062137;
+    } else if ((convertFrom.value === 'centimeter') && (convertTo.value === 'miles')) {
+        result.value = zlength.value * 0.0000062137;
         formulaContent.textContent = hints[6].c_mi;
         result.style.border = '3px solid green';
     }
@@ -115,28 +109,23 @@ function convertValues() {
         result.value = length.value * 100;
         formulaContent.textContent = hints[7].m_c;
         result.style.border = '3px solid green';
-    }
-    else if ((convertFrom.value === 'meter') && (convertTo.value === 'feet')) {
+    } else if ((convertFrom.value === 'meter') && (convertTo.value === 'feet')) {
         result.value = length.value * 3.2808;
         formulaContent.textContent = hints[8].m_f;
         result.style.border = '3px solid green';
-    }
-    else if ((convertFrom.value === 'meter') && (convertTo.value === 'yards')) {
+    } else if ((convertFrom.value === 'meter') && (convertTo.value === 'yards')) {
         result.value = length.value * 1.0936;
         formulaContent.textContent = hints[9].m_y;
         result.style.border = '3px solid green';
-    }
-    else if ((convertFrom.value === 'meter') && (convertTo.value === 'miles')) {
+    } else if ((convertFrom.value === 'meter') && (convertTo.value === 'miles')) {
         result.value = length.value * 0.00062137;
         formulaContent.textContent = hints[10].m_mi;
         result.style.border = '3px solid green';
-    }
-    else if ((convertFrom.value === 'meter') && (convertTo.value === 'inches')) {
+    } else if ((convertFrom.value === 'meter') && (convertTo.value === 'inches')) {
         result.value = length.value * 39.370;
         formulaContent.textContent = hints[11].m_i;
         result.style.border = '3px solid green';
-    }
-    else if ((convertFrom.value === 'meter') && (convertTo.value === 'kilometers')) {
+    } else if ((convertFrom.value === 'meter') && (convertTo.value === 'kilometers')) {
         result.value = length.value / 1000;
         formulaContent.textContent = hints[12].m_km;
         result.style.border = '3px solid green';
